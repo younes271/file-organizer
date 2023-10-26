ddccac2790d51dc93aa8ec253feed91e2cea3194
younes271 <younesschaanoun111@gmail.com>
2023-10-26 22:00:25 +0100

    Add random text to README.md

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# file-organizer
-A new repository created via Python script
-
-
----
-
-Sure, here's a basic Python script that will organize files on your desktop into folders based on their extension type.
-
-```python
 import os
 from pathlib import Path
 
@@ -24,6 +15,3 @@ def organize_directory(directory_path):
             file_path.rename(directory / file_path.name)
 
 organize_directory(DESKTOP_PATH)
-```
-
-This script will create folders for each file type (e.g., .txt, .pdf, .jpg) on your desktop and move the corresponding files into these folders. Please note that this script is simplistic and doesn't handle exceptions or edge cases. It's recommended to back up your files before running it.
